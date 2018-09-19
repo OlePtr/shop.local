4f1061178526c7c41f50989dc166d85d0d2ff441
Юрий <uragorin@yandex.ru>
2018-09-19 20:59:38 +0300

    добавили свойство $config в контроллерах для доступа к настройкам приложения

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -5,9 +5,11 @@ namespace Controllers;
 abstract class Controller
 {
     protected $view;
+    protected $config;
 
     public function __construct ()
     {
         $this->view = new \Libs\View();
+        $this->config = \Libs\Config\Config::getInstance();
     }
 }
