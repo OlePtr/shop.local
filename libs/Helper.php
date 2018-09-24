1b93a26338d026e5b344a4f1e7b6cc3229db0e42
Юрий <uragorin@yandex.ru>
2018-09-24 21:19:52 +0300

    Добавлен вспомогательный класс Helper с функциями для получения данных из массиво ПОСТ, ГЕТ и для редиректа

---
+++ libs/Helper.php
@@ -0,0 +1,29 @@
+<?php
+namespace Libs;
+
+
+class Helper
+{
+    public static function post($key)
+    {
+        if (isset($_POST[$key])) {
+            return $_POST[$key];
+        }
+
+        return null;
+    }
+
+    public static function get($key)
+    {
+        if (isset($_GET[$key])) {
+            return $_GET[$key];
+        }
+
+        return null;
+    }
+
+    public static function redirect($url)
+    {
+        header("Location: {$url}");
+    }
+}
